ba3f5ad2de00d9f7be09626fcae3eed30db7e446
Pete Hering <pjhering@gmail.com>
2020-08-21 10:15:57 -0400

    long overdue

--- src/main/java/org/petehering/lotto/Main.java
+++ src/main/java/org/petehering/lotto/Main.java
@@ -4,6 +4,9 @@ public class Main
 {
     public static void main (String[] args) throws Exception
     {
+        org.petehering.lotto.v1.Main.main(args);
+        org.petehering.lotto.v2.Main.main(args);
+        org.petehering.lotto.v3.Main.main(args);
         org.petehering.lotto.v4.Main.main(args);
         org.petehering.lotto.v5.Main.main(args);
     }
